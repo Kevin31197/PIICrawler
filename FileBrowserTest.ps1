f1b748b76755fce4571f599bf7723f2d00aa7e99
Kevin31197 <60715999+Kevin31197@users.noreply.github.com>
2020-02-24 12:25:08 -0600

    Update FileBrowserTest.ps1

--- FileBrowserTest.ps1
+++ FileBrowserTest.ps1
@@ -1,4 +1,4 @@
-$wordToSearch = @("PASSPORT", "DEPENDENTS", "EFMP", "EXCEPTIONAL FAMILY MEMBER", "DEROS", "OUT OF CYCLE", "ATAAPS", "SOCIAL", "CARDS", "SPOUSE", "SIGNIFICANT OTHER", "DRIVERS LICENSE NUMBER", "OPR", "EPR", "SSN", "SSAN", "SOCIAL ROSTER", "RECALL ROSTER", "ALPHA ROSTER", "DOB", "DATE OF BIRTH", "BANK ROUTING NUMBER", "GAINS ROSTER", "LOSSES", "INSURANCE", "RATER", "RATEE", "UMPR", "REPORTS", "DD577", "AF910", "AF 910", "AF911", "AF 911", "AF912", "AF 912", "LEAVE", "AF707", "AF 707", "AF780", "AF 780", "ADDITIONAL DUTY", "TEST")
+$wordToSearch = @("PASSPORT", "DEPENDENTS", "EFMP", "EXCEPTIONAL FAMILY MEMBER", "DEROS", "OUT OF CYCLE", "ATAAPS", "SOCIAL", "CARDS", "SPOUSE", "SIGNIFICANT OTHER", "DRIVERS LICENSE NUMBER", "OPR", "EPR", "SSN", "SSAN", "SOCIAL ROSTER", "RECALL ROSTER", "ALPHA ROSTER", "DOB", "DATE OF BIRTH", "BANK ROUTING NUMBER", "GAINS ROSTER", "LOSSES", "INSURANCE", "RATER", "RATEE", "UMPR", "REPORTS", "DD577", "AF910", "AF 910", "AF911", "AF 911", "AF912", "AF 912", "LEAVE", "AF707", "AF 707", "AF780", "AF 780", "ADDITIONAL DUTY")
 $wordDocs = @()
 $excelDocs = @()
 $pdfDocs = @()
@@ -19,11 +19,11 @@ function Find-Folders {
         {
         $loop = $false
 		$directory = $browse.SelectedPath
-		#Insert your script here
+		#Script
           Get-ChildItem  -Path $directory -Include "*.txt", "*.docx", "*.doc", "*.xlsx", "*.pdf" -Recurse -ErrorAction SilentlyContinue |`
           ForEach-Object{
                 $file = $_.FullName
-                #Opens, reads, and searches .docx files
+                #Seperates file locations into their respective array
                 if($file -match "(\.docx)$" -or $file -match "(\.doc)$"){
                   $wordDocs += $file
                 }
@@ -37,9 +37,12 @@ function Find-Folders {
                   $txtDocs += $file
                 }                      
           }
+          #Searches files for a match from the word bank
           if($wordDocs){
             $word = New-Object -ComObject Word.application
             foreach ($target in $wordDocs){
+              try{
+
               $document = $word.Documents.Open($target, $false, $true, $false, "ttt")
                     $content = $document.content.Text
                     foreach ($elem in $wordToSearch) 
@@ -57,12 +60,23 @@ function Find-Folders {
                             } 
                         }
                     $document.close($false)
+              }
+              catch [System.Runtime.InteropServices.COMException]{
+                $result = New-Object psobject -Property @{
+                  Location = $target
+                  Type = $_.Exception.Message
+                  Format = "Word"
+              }
+              $out = $out + $result          
+              }
             }
             $word.Quit()
           }
           if($excelDocs){
             $Excel = New-Object -ComObject Excel.Application
             foreach ($target in $excelDocs){
+              try{
+
               $Workbook = $Excel.Workbooks.Open($target, $false, $true)
                     for($i = 1; $i -lt $($Workbook.Sheets.Count() + 1); $i++){
                         $Range = $Workbook.Sheets.Item($i).Range("A:Z")
@@ -80,29 +94,50 @@ function Find-Folders {
                             } 
                         }
                     }
-                    $Workbook.close($false)
+                    $Workbook.close($false)              
+              }
+              catch [System.Runtime.InteropServices.COMException]{
+                $result = New-Object psobject -Property @{
+                  Location = $target
+                  Type = $_.Exception.Message
+                  Format = "Excel"
+              }
+              $out = $out + $result
+              }     
             }
             $Excel.Quit()
           }
           if($pdfDocs){
             $adobe = New-Object -ComObject AcroExch.App
             foreach ($target in $pdfDocs){
-            $PDdoc = New-Object -ComObject AcroExch.PDDoc
-            $PDdoc.Open($target)
-            $AVdoc = $PDdoc.OpenAVDoc("")
-            foreach ($elem in $wordToSearch){
-              if ($AVDoc.FindText($elem, 0, 0, 1)){
-                  $result = New-Object psobject -Property @{
-                      Location = $target
-                      Type = $elem
-                      Format = "PDF"
-                  } 
-                  #$out += $result
-                  $out = $out + $result 
-                  break
-              }                  
-            }
-            $PDdoc.close()
+              try {
+                $PDdoc = New-Object -ComObject AcroExch.PDDoc
+                $PDdoc.Open($target)
+                $AVdoc = $PDdoc.OpenAVDoc("")
+                foreach ($elem in $wordToSearch){
+                  if ($AVDoc.FindText($elem, 0, 0, 1)){
+                      $result = New-Object psobject -Property @{
+                          Location = $target
+                          Type = $elem
+                          Format = "PDF"
+                      } 
+                      #$out += $result
+                      $out = $out + $result 
+                      break
+                  }                  
+                }
+                $PDdoc.close()
+              }
+              catch [System.Runtime.InteropServices.COMException]{
+                $result = New-Object psobject -Property @{
+                  Location = $target
+                  Type = $_.Exception.Message
+                  Format = "PDF"
+              }
+              $out = $out + $result
+              $PDdoc.close()
+              $adobe.CloseAllDocs()
+              }
             }
             $adobe.CloseAllDocs()
             $adobe.exit()
@@ -144,11 +179,7 @@ function Find-Folders {
                 return
             }
         }
-        $out | Sort-Object -Property Location  | Format-Table -Property Type, Location
-        # $txtDocs         
-        # $wordDocs 
-        # $excelDocs 
-        # $pdfDocs
+        $out | Sort-Object -Property Location  | Format-Table -AutoSize -Property Type, Location
     }
     $browse.Dispose()
 } Find-Folders
